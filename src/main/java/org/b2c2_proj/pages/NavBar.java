f83b841d40b58006359b02c5f9dc716331dc8f1b
Lukasz Grenda <grenda-git@outlook.com>
2025-05-11 23:26:14 +0200

    add navbox handling

--- src/main/java/org/b2c2_proj/pages/NavBar.java
+++ src/main/java/org/b2c2_proj/pages/NavBar.java
@@ -3,6 +3,7 @@ package org.b2c2_proj.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -28,6 +29,10 @@ public class NavBar {
         return navbarMenu.findElement(By.className("contact_block"));
     }
 
+    private WebElement getSubscribeButton() {
+        return navbarMenu.findElement(By.className("subscribe_btn"));
+    }
+
     public void clickNavbar(String navbarText) {
         for (WebElement navbarItem : getNavbarItems()) {
             if (navbarItem.getText().equals(navbarText)) {
@@ -50,22 +55,36 @@ public class NavBar {
     }
 
 
+    //tempMethod
     public void printListOfElements() {
         for (WebElement element : getNavbarItems()) {
             System.out.println(element.getText());
         }
     }
 
-    public void printListOfElementsOfElement() {
-        List<WebElement> tmpList;
-        for (WebElement navbarElement : getNavbarItems()) {
-            System.out.println(navbarElement.getText());
-            tmpList = navbarElement.findElements(By.className("dropdown-toggle"));
+    //If needed add text driven method
+    public void clickDropdownItemByIndex(int navboxItemIndex, int dropdownItemIndex) {
+        Actions action = new Actions(driver);
+        WebElement selectedDropdown = getNavbarItems().get(navboxItemIndex);
+        List<WebElement> dropdownItems;
 
-            for (WebElement dropdownElement : tmpList) {
-                System.out.println(dropdownElement.getText());
-            }
+        action.moveToElement(selectedDropdown).perform();
+        dropdownItems = selectedDropdown.findElements(By.className("inner-dropdown"));
+        dropdownItems.get(dropdownItemIndex).click();
+    }
 
+    //tempMethod
+    public void printDropdownItemByIndex(int navboxItemIndex) {
+        Actions action = new Actions(driver);
+        WebElement selectedDropdown = getNavbarItems().get(navboxItemIndex);
+        List<WebElement> dropdownItems;
+
+        System.out.println(selectedDropdown.getText());
+
+        action.moveToElement(selectedDropdown).perform();
+        dropdownItems = selectedDropdown.findElements(By.className("inner-dropdown"));
+        for (WebElement dropdownItem : dropdownItems) {
+            System.out.println(dropdownItem.getText());
         }
     }
 }
