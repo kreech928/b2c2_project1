54b2ca77554a6b650a5825d2de5d3d6a2c6c439f
Lukasz Grenda <grenda-git@outlook.com>
2025-05-12 23:32:35 +0200

    create ContactPage

---
+++ src/main/java/org/b2c2_proj/pages/ContactPage.java
@@ -0,0 +1,13 @@
+package org.b2c2_proj.pages;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class ContactPage {
+    WebDriver driver;
+
+
+    public ContactPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
